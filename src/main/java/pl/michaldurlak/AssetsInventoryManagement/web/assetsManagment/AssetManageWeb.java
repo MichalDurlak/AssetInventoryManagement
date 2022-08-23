59d5b94be06819c990491396aa8eb7c3be63fa38
Michał Durlak <durlak.m@wp.pl>
2022-08-23 23:54:12 +0200

    Created test endpoint that gets param

--- src/main/java/pl/michaldurlak/AssetsInventoryManagement/web/assetsManagment/AssetManageWeb.java
+++ src/main/java/pl/michaldurlak/AssetsInventoryManagement/web/assetsManagment/AssetManageWeb.java
@@ -1,11 +1,29 @@
 package pl.michaldurlak.AssetsInventoryManagement.web.assetsManagment;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import pl.michaldurlak.AssetsInventoryManagement.web.basics.NavbarLayout;
+import org.springframework.web.bind.annotation.*;
 
-@Route(value = "asset/{id}", layout = NavbarLayout.class)
+@RestController
+@RequestMapping("/asset")
 @PageTitle("{id} info")
 public class AssetManageWeb extends VerticalLayout {
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public void  getDeatailsOfAsset(@PathVariable Long id) {
+        getAssetManageWeb(id);
+    }
+
+    public AssetManageWeb() {
+    }
+
+    public void getAssetManageWeb(Long id){
+        TextField textFieldId = new TextField(id.toString());
+        add(textFieldId);
+    }
+
 }
+
+
